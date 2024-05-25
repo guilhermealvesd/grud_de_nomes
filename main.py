efdf964e8b65cec840185ac13742419ee82b3072
guilhermealvesd <alvessig@gmail.com>
2024-05-24 21:50:40 -0300

    Versão 1.0.

---
+++ main.py
@@ -0,0 +1,27 @@
+"""
+Crie um programa que tenha as opções:
+
+- Cadastrar pessoa
+- Lista pessoas cadastradas
+- Pesquisar pelo nome de uma pessoa
+- Ordenar a lista por ordem alfabética
+- Atualizar nome
+- Deletar nome da lista
+- Sair do porgrama
+
+"""
+
+#Exibe a lista de funcionalidades
+
+print(f'{'-'*30} FUNCIONALIDADES {'-'*30}\n')
+
+print('Digite 1: para INSERIR uma pessoa na lista.')
+print('Digite 2: para LISTAR as pessoas cadastradas.')
+print('Digite 3: para PESQUISAR uma pessoa na lista.')
+print('Digite 4: para ATUALIZAR um nome da lista.')
+print('Digite 5: para DELETAR uma pessoa na lista.')
+print('Digite 6: para SAIR.')
+
+#Função INSERIR
+
+lista = []
